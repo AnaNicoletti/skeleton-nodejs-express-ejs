9460e561ef3cdc172c8f80392e99737c8b283815
Ana Nicoletti <86062663+AnaNicoletti@users.noreply.github.com>
2022-03-16 20:56:16 +0000

    Criada a rota UPDATE (atualizar), para atualizar autores

--- models/autor.js
+++ models/autor.js
@@ -12,5 +12,12 @@ class Autor {
     const values = [data.nome, data.sobrenome, data.datanascimento]
     return await connect.query(sql, values);
   }
+
+  static async atualizar(data, id){
+    const connect = await db.connect();
+    const sql = "update autores set nome = $1, sobrenome = $2, data_de_nascimento = $3 where id = $4";
+    const values = [data.nome, data.sobrenome, data.datanascimento, id]
+    return await connect.query(sql, values);
+  }
 }
 module.exports = Autor;
--- routes/index.js
+++ routes/index.js
@@ -18,4 +18,14 @@ router.get('/inserir', async function(req, res, next) {
   res.json(autores.rows);
 });
 
+router.get('/atualizar', async function(req, res, next) {
+  let autor = {
+    nome:"Ana",
+    sobrenome:"Nicoletti",
+    datanascimento:"2002-09-11"
+  };
+  const autores = await Autor.atualizar(autor, 6);
+  res.json(autores.rows);
+});
+
 module.exports = router;
